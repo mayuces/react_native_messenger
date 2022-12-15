6c6b3e3c71cea83ab4706372fd7d05dae1e311b5
M.Ali Yücesoylu <myucesoylu@gmail.com>
2022-12-15 18:34:08 +0300

    Initial commit
    Generated by create-expo-app 1.1.4.

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAxmncmYD7GCNXuMY7nEu_b1GNFMYpCnx4",
+  authDomain: "signal-clone-3295f.firebaseapp.com",
+  projectId: "signal-clone-3295f",
+  storageBucket: "signal-clone-3295f.appspot.com",
+  messagingSenderId: "591016292945",
+  appId: "1:591016292945:web:84606894626faee25bbe70"
+};
